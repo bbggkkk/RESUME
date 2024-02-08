185fe2d412dd7fdbae2eb97b582872c9964076fb
bbggkkk <bbggkkk@naver.com>
2024-02-08 13:26:11 +0900

    도메인 업데이트

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `하나나킥`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://bbggkkk.github.io/RESUME/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
